17821992bc4777d4948296381904e43809e80eea
Ajith16102001 <kajithkarthikeyan@gmail.com>
2026-04-10 02:20:44 -0700

    Update Footer.jsx

--- project-selling-frontend/src/components/Footer.jsx
+++ project-selling-frontend/src/components/Footer.jsx
@@ -5,9 +5,7 @@ export default function Footer() {
     <footer className="text-center text-gray-500 py-6 text-sm">
       © 2025 AK Tech Solution. All rights reserved.
       <div className="mt-2">
-        <Link to="/admin/login" className="hover:text-white">
-          Admin Login
-        </Link>
+        
       </div>
     </footer>
   );
